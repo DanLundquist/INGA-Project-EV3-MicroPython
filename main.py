1e051e10cb136b33e84f8a13a00fd3621f7a9dc3
Daniel L <73555530+DanLundquist@users.noreply.github.com>
2020-10-29 13:13:35 +0100

    Update main.py
    Updated.

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-!/usr/bin/env pybricks-micropython
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
-ev3 = EV3Brick()
-#test_motor = Motor(Port.B,Port.A)
-test_motor = Motor(Port.A)
+from pybricks.robotics import DriveBase
 
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.A)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Go forward and backwards for one meter.
+robot.straight(1000)
 ev3.speaker.beep()
-#test_motor.run_target(500,360)
-test_motor.run_target(500,360)
-ev3.speaker.beep(1000,500)
+
+robot.straight(-1000)
+ev3.speaker.beep()
+
+# Turn clockwise by 360 degrees and back again.
+robot.turn(360)
+ev3.speaker.beep()
+
+robot.turn(-360)
+ev3.speaker.beep()
+
+
 
